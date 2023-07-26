361056c3fe7f0e5421381184446059a4667b1b61
deeplow <deeplower@protonmail.com>
2023-07-26 12:43:00 +0100

    Base64 encode externally contributed docs
    Prevent the accidental opening of externally implemented documents
    since we can't rebuild them. This ensures that it's only ran through
    dangerzone.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,13 +1,16 @@
 import sys
-from pathlib import Path
-
 import pytest
 
-sys.dangerzone_dev = True  # type: ignore[attr-defined]
+from pathlib import Path, PosixPath
+from typing import List
 
 from dangerzone.document import SAFE_EXTENSION
 
+sys.dangerzone_dev = True  # type: ignore[attr-defined]
+
+
 SAMPLE_DIRECTORY = "test_docs"
+SAMPLE_EXTERNAL_DIRECTORY = "test_docs_external"
 BASIC_SAMPLE = "sample-pdf.pdf"
 test_docs_dir = Path(__file__).parent.joinpath(SAMPLE_DIRECTORY)
 test_docs = [
@@ -19,6 +22,31 @@ test_docs = [
 
 # Pytest parameter decorators
 for_each_doc = pytest.mark.parametrize("doc", test_docs)
+
+
+# External Docs - base64 docs encoded for externally sourced documents
+# XXX to reduce the chance of accidentally opening them
+test_docs_external_dir = Path(__file__).parent.joinpath(SAMPLE_EXTERNAL_DIRECTORY)
+
+
+def get_docs_external(pattern: str = "*") -> List[PosixPath]:
+    if not pattern.endswith("*"):
+        pattern = f"{pattern}.b64"
+    return [
+        p
+        for p in test_docs_external_dir.rglob(pattern)
+        if p.is_file() and not (p.name.endswith(SAFE_EXTENSION))
+    ]
+
+
+# Pytest parameter decorators
+def for_each_external_doc(glob_pattern: str = None):
+    test_docs_external = get_docs_external(glob_pattern)
+    return pytest.mark.parametrize(
+        "doc",
+        test_docs_external,
+        ids=[str(doc.name).rstrip(".b64") for doc in test_docs_external],
+    )
 
 
 class TestBase:
